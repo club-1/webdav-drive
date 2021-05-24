82723f29a55ab0fd34710862d225a4e8929a0f36
n-peugnet <n.peugnet@free.fr>
2021-05-24 15:35:49 +0200

    feat: add all properties from posix extension

--- src/module/PosixModule.ts
+++ src/module/PosixModule.ts
@@ -1,16 +1,50 @@
 import type { Core, Module } from "../main/Core";
-import { Directory } from "../model/Files";
 
 type PosixProperties = {
-	links: number,
+	mode: number,
+	user: string,
+	group: string,
+	atime: number,
+	ctime: number,
+	link?: string,
 }
 
 export class PosixModule implements Module {
 	init(core: Core) {
-		core.registerInodeProperty("links", {
-			 read: (i): number => {
+		core.registerInodeProperty("mode", {
+			read: (i): string => {
 				let props = i.raw as PosixProperties;
-				return props.links;
+				return props.mode.toString(8);
+			},
+		});
+		core.registerInodeProperty("user", {
+			read: (i): string => {
+				let props = i.raw as PosixProperties;
+				return props.user;
+			},
+		});
+		core.registerInodeProperty("group", {
+			read: (i): string => {
+				let props = i.raw as PosixProperties;
+				return props.group;
+			},
+		});
+		core.registerInodeProperty("atime", {
+			read: (i): Date => {
+				let props = i.raw as PosixProperties;
+				return new Date(props.atime * 1000);
+			},
+		});
+		core.registerInodeProperty("ctime", {
+			read: (i): Date => {
+				let props = i.raw as PosixProperties;
+				return new Date(props.ctime * 1000);
+			},
+		});
+		core.registerInodeProperty("link", {
+			read: (i): string | false => {
+				let props = i.raw as PosixProperties;
+				return props.link || false;
 			},
 		});
 	}
