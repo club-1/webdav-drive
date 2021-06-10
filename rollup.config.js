572a45a8e22bfa6672357d81a85343f37ea5572c
n-peugnet <n.peugnet@free.fr>
2021-06-10 20:13:31 +0200

    build: separate watch script from start one

--- rollup.config.js
+++ rollup.config.js
@@ -10,27 +10,6 @@ import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
-
 export default {
 	input: 'src/main.ts',
 	output: {
@@ -69,10 +48,6 @@ export default {
 		typescript({
 			inlineSources: !production
 		}),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
