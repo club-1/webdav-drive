3af757c28be7c601c92f2319ca00026e99f7d8ce
n-peugnet <n.peugnet@free.fr>
2022-02-27 19:58:04 +0100

    feat: setup app for dynamic module imports
    - rollup: use native es format
    - index: use native es module script
    - typescript: use es2020 module format output

--- rollup.config.js
+++ rollup.config.js
@@ -14,9 +14,9 @@ export default {
 	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
-		format: 'iife',
+		format: 'es',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		dir: 'public/build/',
 	},
 	plugins: [
 		svelte({
