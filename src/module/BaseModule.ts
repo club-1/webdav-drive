de97c6bb7ec3baea448011983f87ff155c391b2b
n-peugnet <n.peugnet@free.fr>
2022-02-27 17:05:28 +0100

    fix: add GPL-3.0+ license

--- src/module/BaseModule.ts
+++ src/module/BaseModule.ts
@@ -1,6 +1,23 @@
+/*
+	This file is part of WebDAV-Drive.
+
+	Copyright 2021, 2022  Nicolas Peugnet<n.peugnet@free.fr>
+
+	WebDAV-Drive is free software: you can redistribute it and/or modify it under
+	the terms of the GNU General Public License as published by the Free Software
+	Foundation, either version 3 of the License, or (at your option) any later
+	version.
+
+	WebDAV-Drive is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with
+	WebDAV-Drive. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import type { Core, Module } from "../main/Core";
 import { File, type InodeOperation } from "../model/Files";
-
 
 const list: InodeOperation<Map<string,unknown>> = (i, map) => {
 	map.set("path", i.path);
--- src/view/FileListCell.svelte
+++ src/view/FileListCell.svelte
@@ -1,3 +1,20 @@
+<!--
+	This file is part of WebDAV-Drive.
+
+	Copyright 2021, 2022  Nicolas Peugnet<n.peugnet@free.fr>
+
+	WebDAV-Drive is free software: you can redistribute it and/or modify it under
+	the terms of the GNU General Public License as published by the Free Software
+	Foundation, either version 3 of the License, or (at your option) any later
+	version.
+
+	WebDAV-Drive is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with
+	WebDAV-Drive. If not, see <https://www.gnu.org/licenses/>.
+-->
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import { File } from "../model/Files";
