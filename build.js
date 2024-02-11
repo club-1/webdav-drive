efff8d1aa01ddc476a0c1428b1015043296a2dc4
Nicolas Peugnet <nicolas@club1.fr>
2024-02-11 13:00:40 +0100

    build: esbuild plugin to bundle fonts

--- build.js
+++ build.js
@@ -4,20 +4,56 @@ import esbuild from "esbuild";
 import esbuildSvelte from "esbuild-svelte";
 import { sveltePreprocess } from "svelte-preprocess";
 import { optimizeImports } from "carbon-preprocess-svelte";
+import { join } from "path";
+import { readFile, writeFile } from "fs/promises";
+import { existsSync, mkdirSync } from "fs";
 
-esbuild
-  .build({
-    entryPoints: ["src/main.ts"],
-    bundle: true,
-    outdir: "./public/app",
-    plugins: [
-      esbuildSvelte({
-        preprocess: [
-          sveltePreprocess(),
-          optimizeImports(),
-        ],
-      }),
-    ],
-  })
-  .catch(() => process.exit(1))
-
+await esbuild.build({
+  entryPoints: [
+    "src/main.ts",
+    "src/module/BaseModule.ts",
+    "src/module/PosixModule.ts",
+  ],
+  outdir: "./public/app",
+  format: "esm",
+  bundle: true,
+  minify: true,
+  splitting: true,
+  logLevel: "info",
+  plugins: [
+    esbuildSvelte({
+      preprocess: [
+        sveltePreprocess(),
+        optimizeImports(),
+      ],
+    }),
+    {
+      name: 'download-fonts',
+      setup(build) {
+        const cache = '.cache';
+        mkdirSync(cache, { recursive: true });
+        build.onResolve({ filter: /^https?:\/\/.+\.(woff2?|ttf)/ }, args => {
+          return {
+            path: args.path,
+            namespace: 'download-fonts',
+          }
+        });
+        build.onLoad({ filter: /.*/, namespace: 'download-fonts' }, async args => {
+          const key = join(cache, encodeURIComponent(args.path));
+          let buf;
+          if (existsSync(key)) {
+            buf = readFile(key);
+          } else {
+            const res = await fetch(args.path);
+            buf = res.arrayBuffer();
+            await writeFile(key, Buffer.from(await buf));
+          }
+          return {
+            contents: new Uint8Array(await buf),
+            loader: 'file',
+          }
+        });
+      },
+    },
+  ],
+})
